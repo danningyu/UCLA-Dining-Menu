30d06242caea53005f4a0be12718ff3b7fdf4d24
danningyu <danninglyu@gmail.com>
2019-10-14 14:33:20 -0700

    Update

--- request.py
+++ request.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from enum import Enum
 from datetime import date, datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+import collections
 
 sched = BlockingScheduler()
 TEST_URL = 'http://menu.dining.ucla.edu/Menus'
@@ -33,48 +34,42 @@ def is_good_response(resp):
 def log_error(e):
 	print(e)
 
-def process_data(html_file):
+Items_And_Date = collections.namedtuple('Items_And_Date', ['items', 'date'])
+
+def separate_by_meal_period(html_file):
+	#takes in html page, and places menu items in appropriate list
+	#returns list of lists, one list for each meal period
 	soup = BeautifulSoup(html_file, 'html.parser')
-	all_links = soup.find_all('a')
-	i=0
+	dates_and_items = soup.find_all(['h2', 'h3', 'a']) #h2 for date, a for menu items
+	# print(dates_and_items)
+	# for num, item in enumerate(dates_and_items):
+		# print(str(num) + str(item))
 	breakfast_items = []
 	lunch_items = []
+	brunch_items = []
 	dinner_items = []
-	brunch_flag=False
-	food_items = [breakfast_items, lunch_items, dinner_items]
-	index = 0
-	all_links = all_links[41:] #eliminate headers
-	all_links = all_links[:-5] #and footers
-	for link in all_links:
-		i=i+1
+	food_items = [breakfast_items, brunch_items, lunch_items, dinner_items]
+	dates_and_items = dates_and_items[41:] #eliminate headers
+	dates_and_items = dates_and_items[:-5] #and footers
+	index=-1
+	date_time_obj = None
+	for link in dates_and_items:
+		if(link.contents[0].find('for') != -1):
+			contains_date = link.contents[0]
+			menu_date = contains_date[contains_date.find(',')+2:]
+			date_time_obj = datetime.strptime(menu_date, '%B %d, %Y')
+			print('Extracted date: ' + str(date_time_obj))
 		if any('Detailed Breakfast' in s for s in link):
 			index = 0
-		elif any('Detailed Lunch' in s for s in link):
+		elif any('Detailed Brunch' in s for s in link):
 			index = 1
-		elif any('Detailed Dinner' in s for s in link):
+		elif any('Detailed Lunch' in s for s in link):
 			index = 2
+		elif any('Detailed Dinner' in s for s in link):
+			index = 3
 		food_items[index].append(link.contents[0]+'\n')
-	return food_items
-
-def add_locations(listOfItems, time):
-	i=0
-	index=0
-	tempList = listOfItems.copy()
-	if time == 0:	#breakfast	
-		for items in tempList:
-			if "Detailed Menu" in items and i==0:
-				listOfItems.insert(index, '\n-----'+str(dining_hall_key[1])+'-----\n')
-				i = i+1
-			elif "Detailed Menu" in items and i==1:
-				listOfItems.insert(index, '\n-----'+str(dining_hall_key[3])+'-----\n')
-				i = i+1
-			index = index+1
-	elif time == 1 or time == 2:
-		for items in tempList:
-			if "Detailed Menu" in items:
-				listOfItems.insert(index, '\n-----'+str(dining_hall_key[i])+'-----\n')
-				i = i+1
-			index = index+1
+	return_val = Items_And_Date(food_items, date_time_obj)
+	return return_val
 
 def export_data():
 	print('Running program...')
@@ -82,28 +77,29 @@ def export_data():
 	current_time = datetime.now().time()
 	output = simple_get(TEST_URL) #get the webpage
 	print('Exporting data...')
-	all_items = process_data(output)
-	current_time=str(current_time.hour)+'_'+str(current_time.minute)+'_'+str(current_time.second)
-	time=0
-	for meal_items in all_items:
-		add_locations(all_items[time], time)
-		time = time + 1
-	filename = "menu"+current_time+".txt"
+	all_items_and_date = separate_by_meal_period(output)
+	all_items = all_items_and_date[0]
+	#for meal_period_items in all_items_and_date[0]:
+		#add_locations(meal_period_items)
+
+	filename = "menu.txt"
 	file1 = open(filename, "w+")
 	file1.writelines('UCLA Dining menu for ' + str(current_day) + '\n')
-	file1.writelines('Generated on ' + str(current_time.replace('_', ':'))+ ' PDT\n\n')
+	file1.writelines('Generated on PDT\n\n')
 	file1.writelines(all_items[0])
 	file1.write('\n\n')
 	file1.writelines(all_items[1])
 	file1.write('\n\n')
 	file1.writelines(all_items[2])
+	file1.write('\n\n')
+	file1.writelines(all_items[3])
 	file1.close()
 	print('Done running program...')
 
 #run once upon startup
 export_data()
 
-@sched.scheduled_job('interval', seconds=10)
+@sched.scheduled_job('interval', seconds=600)
 def timed_job():
     print('Executing job on ' + str(datetime.now().time()))
     export_data()
